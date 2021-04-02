e15baa9830fea92a87267fa18203d5d99391ddea
Angel Moreno <angelmrsofa@gmail.com>
2021-04-02 15:09:08 -0600

    finished anecdotes project

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+const initialState = [
+  {
+    anecdote: 'If it hurts, do it more often',
+    votes: 2
+  },
+  {
+    anecdote:'Adding manpower to a late software project makes it later!',
+    votes: 3
+  },
+  {
+    anecdote:'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    votes: 12
+  },
+  {
+    anecdote:'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    votes: 10
+  },
+  {
+    anecdote:'Premature optimization is the root of all evil.',
+    votes: 4
+  },
+  {
+    anecdote:'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    votes: 8
+  },
+]
 
-function App() {
+const App = () => {
+  const [anecdotes, setanecdotes] = useState(initialState)
+  const [selected, setSelected] = useState(0)
+
+  const nextAnecdote = () => void 
+    setSelected(prev => (prev + 1) % anecdotes.length)
+  
+  const voteanecdote = () => {
+    const newanecdotes = [...anecdotes]
+    newanecdotes[selected].votes += 1
+    setanecdotes(newanecdotes)
+  }
+
+  const majorAnecdoteVotes = () => {
+    return anecdotes.reduce((major, anecdote) => {
+      if(anecdote.votes > major.votes) {
+        major = anecdote
+        return major
+      }
+      return major
+    }, {votes: 0})
+  }
+  const majorAnecdote = majorAnecdoteVotes()
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ margin: "2rem" }}>
+      <p>"{anecdotes[selected].anecdote}"</p>
+      <p>has {anecdotes[selected].votes} votes</p>
+      <button onClick={voteanecdote} >vote</button>
+      <button onClick={nextAnecdote}>next anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      <p>"{majorAnecdote.anecdote}"</p>
+      <p>has {majorAnecdote.votes} votes</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
